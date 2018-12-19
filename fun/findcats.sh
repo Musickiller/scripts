a2d351cf6826d1000418cb2be7bd8225d2873391
Musickiller <musickiller@yandex.ru>
2018-12-19 13:04:25 +0300

    added credits o_O

--- fun/findcats.sh
+++ fun/findcats.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# Credits:
+# Proposed by [@gardner](@tgan+eDWsq+xJF43JyV6aR2Gb8pf8xEXTa2GLzT7Sgc=.ed25519)
+#  ...and [@mixmix](@ye+QM09iPcDJD6YvQYjoQc7sLF/IFhmNbEqgdzQo3lQ=.ed25519)
 find . -type f -name \*cat\*.gif | xargs firefox
