1ece1857b5ba4613c016fe6856db129801696e6f
Musickiller <musickiller@yandex.ru>
2019-01-24 09:18:31 +0300

    add palemoon folders, add more variables
    + palemoon
    
    ---
    
    + %windir%
    + %localappdata%
    + %systemdrive%

--- bats/RedButt.bat
+++ bats/RedButt.bat
@@ -5,19 +5,21 @@ REM Roaming
 RD /S /Q "%APPDATA%\Mozilla\Firefox\"
 RD /Q /S "%APPDATA%\Waterfox"
 RD /Q /S "%APPDATA%\CLIQZ"
+RD /Q /S "%APPDATA%\Moonchild Productions\Pale Moon"
 REM Local
-RD /Q /S "C:\Users\%USERNAME%\AppData\Local\Mozilla\Firefox\"
-RD /Q /S "C:\Users\%USERNAME%\AppData\Local\CLIQZ"
-RD /Q /S "C:\Users\%USERNAME%\AppData\Local\Waterfox"
+RD /Q /S "%localappdata%\Mozilla\Firefox\"
+RD /Q /S "%localappdata%\CLIQZ"
+RD /Q /S "%localappdata%\Waterfox"
+RD /Q /S "%localappdata%\Moonchild Productions\Pale Moon\Profiles"
 
 REM Chrome-like
-RD /Q /S "C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data"
+RD /Q /S "%systemdrive%\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data"
 
 REM Other
-RD /Q /S "C:\Users\%USERNAME%\AppData\Local\Opera Software\Opera Stable\Cache"
-RD /Q /S "C:\Users\%USERNAME%\AppData\Local\qBittorrent\logs"
-RD /Q /S "C:\Users\%USERNAME%\AppData\Local\qBittorrent\BT_backup"
+RD /Q /S "%systemdrive%\Users\%USERNAME%\AppData\Local\Opera Software\Opera Stable\Cache"
+RD /Q /S "%systemdrive%\Users\%USERNAME%\AppData\Local\qBittorrent\logs"
+RD /Q /S "%systemdrive%\Users\%USERNAME%\AppData\Local\qBittorrent\BT_backup"
 
 REM Windows Default
-DEL /S /Q /F "C:\Windows\Downloaded Program Files"
-DEL /S /Q /F "C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\Temporary Internet Files"
+DEL /S /Q /F "%windir%\Downloaded Program Files"
+DEL /S /Q /F "%localappdata%\Microsoft\Windows\Temporary Internet Files"
